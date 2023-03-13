756dde56f133792230e17a912e60e7a326ae5b96
能量超正 <1006436389@qq.com>
2023-03-13 11:13:48 +0800

    添加了复制文本的公共方法

--- source/common/index.ts
+++ source/common/index.ts
@@ -47,3 +47,28 @@ export function throttle(fn: (...arg: any[]) => void, delay = 500) {
 export function deepClone(data: any) {
   return JSON.parse(JSON.stringify(data))
 }
+
+/***
+ *  复制文本的方法
+ * @param text 要复制的文本内容
+ */
+export function copyText(text: string) {
+  var input = document.createElement('input')
+  /**
+   * 现象：ios点击复制时屏幕下方会出现白屏抖动
+   * 具体问题：输入框拉起键盘又瞬间收起
+   * 解决：设置输入框为只读
+   */
+  input.setAttribute('readonly', 'readonly')
+  input.setAttribute('value', text)
+  document.body.appendChild(input)
+  /**
+   * 现象：ios无法复制
+   * 具体问题：由于 input.select() 在ios下并没有选中全部内容
+   * 解决：input.setSelectionRange(0, 9999)选中所有内容
+   */
+  input.setSelectionRange(0, 9999)
+  input.select() // 选择对象
+  document.execCommand('Copy')
+  document.body.removeChild(input)
+}
--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,9 @@
-export { debounce, throttle, deepClone, toLowerCase } from './common/index'
+export {
+  debounce,
+  throttle,
+  deepClone,
+  toLowerCase,
+  copyText,
+} from './common/index'
 export { formatTime, formatTimeArray, parseTime } from './time/index'
 export { _debounce, _throttle } from './special/index'
